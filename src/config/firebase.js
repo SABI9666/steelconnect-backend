f4c6c3b883c6622a29412fd8ebdb0603f986e610
sabin <sabincn676@gmail.com>
2025-07-30 14:37:54 +0530

    Initial commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -18,7 +18,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     // --- FIX: Explicitly set the correct bucket name ---
-    storageBucket: 'steelconnect-backend-3f684.appspot.com'
+    storageBucket: 'steelconnect-backend-3f684.firebasestorage.app'
   });
 }
 

cbb21e79ceebc97b3f1642f218438b914291ff66
SABI9666 <sabincn676@gmail.com>
2025-08-16 02:38:51 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -17,8 +17,8 @@ const serviceAccount = JSON.parse(serviceAccountJson);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    // --- FIX: The bucket name should not include 'firebasestorage.' ---
-    storageBucket: 'steelconnect-backend-3f684.firebasestorage.app'
+    // --- CORRECTED BUCKET NAME ---
+    storageBucket: 'steelconnect-backend-3f684.appspot.com'
   });
 }
 

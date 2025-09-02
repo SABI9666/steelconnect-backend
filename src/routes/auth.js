39a97fceb5aeb329bca27aae4b65fc955276ed48
SABI9666 <sabincn676@gmail.com>
2025-09-03 02:43:10 +0530

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { adminDb } from '../config/firebase.js';
 
@@ -752,5 +752,6 @@ router.get('/verify', async (req, res) => {
     });
   }
 });
+
 
 export default router;

6c345ec9422a1d2cf5004ffdb03f63c4ff770683
SABI9666 <sabincn676@gmail.com>
2025-09-07 11:52:43 +0530

    Update messages.js

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -6,7 +6,7 @@ import {
   getMessages
 } from '../controllers/messageController.js';
 import { authenticateToken } from '../middleware/auth.js';
-import { NotificationService } from './notifications.js';
+import { NotificationService } from '../services/notificationService.js';
 
 const router = express.Router();
 
